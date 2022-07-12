b8d91990fcd31c81581d8cad8537deb2eb28676c
song-cool <oscar2978@naver.com>
2022-07-12 10:29:45 +0900

    스토리보드 수정

--- TossClone2/HomeVC.swift
+++ TossClone2/HomeVC.swift
@@ -7,33 +7,18 @@
 
 import UIKit
 
-class HomeVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class HomeVC: UIViewController {
     
-    let cellIdentifier = "bankCell"
-    let cellData = ["KB은행통장","하나통장","신한 주거래 우대통장"]
+
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        100.0
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = self.homeTableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        cell.textLabel?.text = cellData[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cellData.count
-    }
-    
-    @IBOutlet weak var bankName: UILabel!
-    @IBOutlet weak var homeTableView: UITableView!
+//    @IBOutlet weak var BankStackView: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        homeTableView.delegate = self
-        homeTableView.dataSource = self
+
+//        BankStackView.layer.cornerRadius = 100
+//        BankStackView.clipsToBounds = true
     }
     
     
